ee0fc1c441298d196e2514f78bb8b5f888c6ffa8
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-04-26 02:48:27 +0000

    Réglage du zip_safe sur les modules qui en ont besoin
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6919 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(name='vigilo-connector-metro',
         license='http://www.gnu.org/licenses/gpl-2.0.html',
         long_description="Gateway from the Vigilo message bus (XMPP) "
                          "to RRD files.",
+        zip_safe=False, # pour pouvoir écrire le dropin.cache de twisted
         install_requires=[
             'setuptools',
             'vigilo-common',
