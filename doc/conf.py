6bc8ec9add67819bf1dd4182fe87f3456b63de92
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-09-24 18:20:43 +0200

    Doc: génération PDF
    Change-Id: I4d9740ec375699ee7bc9e516f71ea71c2abdbd86
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/298
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>

---
+++ doc/conf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+project = u'Vigilo connector-metro'
+
+pdf_documents = [
+        ('admin', "connector-metro-admin", "Connector-metro : Administration", u'Vigilo'),
+]
+
+execfile("../buildenv/doc/conf.py")
