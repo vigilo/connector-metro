82123534761c01cb45f1cf9e17cad9e82616d5c9
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-18 16:31:52 +0200

    Activation de la doc PDF / Latex
    Change-Id: I135d28e54a6b21e39c2ccd9637b3caa16756ed3d

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,12 @@
 project = u'Vigilo connector-metro'
 
 pdf_documents = [
-        ('admin', "connector-metro-admin", "Connector-metro : Guide d'administration", u'Vigilo'),
+        ('admin', "admin-connector-metro", "Connector-metro : Guide d'administration", u'Vigilo'),
+]
+
+latex_documents = [
+        ('admin', 'admin-connector-metro.tex', u"Connector-metro : Guide d'administration",
+         'AA100004-2/ADM00001', 'vigilo'),
 ]
 
 execfile("../buildenv/doc/conf.py")
