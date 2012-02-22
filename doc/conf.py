780780b3e286b3404c33cf06ccec330516177ed9
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-22 15:11:30 +0100

    Factorisation de la doc
    Change-Id: I83a6cf2303e612eb95690db154cbef1752cbccad

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
-project = u'Vigilo connector-metro'
+name = u'connector-metro'
+
+project = u'Vigilo %s' % name
 
 pdf_documents = [
-        ('admin', "admin-connector-metro", "Connector-metro : Guide d'administration", u'Vigilo'),
+        ('admin', "admin-%s" % name, "Connector-metro : Guide d'administration", u'Vigilo'),
 ]
 
 latex_documents = [
-        ('admin', 'admin-connector-metro.tex', u"Connector-metro : Guide d'administration",
+        ('admin', 'admin-%s.tex' % name, u"Connector-metro : Guide d'administration",
          'AA100004-2/ADM00001', 'vigilo'),
 ]
 
