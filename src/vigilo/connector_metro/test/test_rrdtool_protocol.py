80368fedd4f53c3a88e8c4c85baee2fb2caef8b4
Francois POIROTTE <francois.poirotte@c-s.fr>
2016-03-02 10:13:14 +0100

    Mise à jour des copyrights
    Change-Id: I9eac0384106313b98b4975bc8756e4fc260eb9c9
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1948
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/connector_metro/test/test_rrdtool_protocol.py
+++ src/vigilo/connector_metro/test/test_rrdtool_protocol.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: set et sw=4 ts=4 ai:
 # pylint: disable-msg=R0904,C0111,W0613
-# Copyright (C) 2006-2015 CS-SI
+# Copyright (C) 2006-2016 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 from __future__ import absolute_import
