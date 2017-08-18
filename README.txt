a6f80a7e60937a480663a87b3516289c04b13dd5
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-08-18 12:01:40 +0200

    [#1666] Mise à jour des adresses de contact
    Change-Id: I15dfbdcb510144cef89f228e7ac3b37647cd423b
    Refs: #1666

--- README.txt
+++ README.txt
@@ -35,7 +35,7 @@ Connector Metro est sous licence `GPL v2`_.
 
 
 .. _documentation officielle: Vigilo_
-.. _Vigilo: http://www.projet-vigilo.org
+.. _Vigilo: http://www.vigilo-nms.com
 .. _RRDtool: http://oss.oetiker.ch/rrdtool
 .. _GPL v2: http://www.gnu.org/licenses/gpl-2.0.html
 
--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ def install_i18n(i18ndir, destdir):
 setup(name='vigilo-connector-metro',
         version='4.1.0rc1',
         author='Vigilo Team',
-        author_email='contact@projet-vigilo.org',
-        url='http://www.projet-vigilo.org/',
+        author_email='contact.vigilo@c-s.fr',
+        url='http://www.vigilo-nms.com/',
         description="Vigilo Metrology connector",
         license='http://www.gnu.org/licenses/gpl-2.0.html',
         long_description="Store performance data from the Vigilo "
