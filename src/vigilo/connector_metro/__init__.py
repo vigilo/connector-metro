73b91d28761c6497c2608671ba04096b974b4a95
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-07-08 11:11:03 +0200

    [#1913] Eclatement connexion au bus AMQP
    Utilisation de 2 connexions séparées avec le bus AMQP
    (une pour la lecture, l'autre pour l'écriture).
    
    Change-Id: I8164116dda60b27f19f4040d6329d105f04b1d63
    Refs: #1913

--- src/vigilo/connector_metro/__init__.py
+++ src/vigilo/connector_metro/__init__.py
@@ -35,8 +35,14 @@ def makeService(options):
     root_service = service.MultiService()
 
     # Client du bus
-    client = client_factory(settings)
-    client.setServiceParent(root_service)
+    client_in = client_factory(settings)
+    client_in.setName("vigilo_client_in")
+    client_in.setServiceParent(root_service)
+
+    client_out = client_factory(settings)
+    client_out.setName("vigilo_client_out")
+    client_out.setServiceParent(root_service)
+
     providers = []
 
     # Configuration
@@ -71,7 +77,7 @@ def makeService(options):
     # Gestion des seuils
     if must_check_th:
         threshold_checker = ThresholdChecker(rrdtool, confdb)
-        bus_publisher = buspublisher_factory(settings, client)
+        bus_publisher = buspublisher_factory(settings, client_out)
         bus_publisher.registerProducer(threshold_checker, streaming=True)
         providers.append(bus_publisher)
     else:
@@ -79,7 +85,7 @@ def makeService(options):
 
     # Gestionnaire principal des messages
     bustorrdtool = BusToRRDtool(confdb, rrdtool, threshold_checker)
-    bustorrdtool.setClient(client)
+    bustorrdtool.setClient(client_in)
     subs = parseSubscriptions(settings)
     queue = settings["bus"]["queue"]
     queue_messages_ttl = int(settings['bus'].get('queue_messages_ttl', 0))
@@ -90,7 +96,7 @@ def makeService(options):
     # Statistiques
     from vigilo.connector.status import statuspublisher_factory
 
-    status_publisher = statuspublisher_factory(settings, client,
+    status_publisher = statuspublisher_factory(settings, client_out,
                                                providers=providers)
 
     return root_service
